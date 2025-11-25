816ac58f5a628f6bafb3977768b0326ffac04c00
Deedepya <dedeepyaitech@gmail.com>
2025-11-25 12:11:43 -0600

    removed ios files

---
+++ ios/OnboardingiOSApp/OnboardingiOSApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OnboardingiOSApp
+//
+//  Created by dedeepya reddy salla on 25/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/OnboardingiOSApp/OnboardingiOSApp/OnboardingiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingiOSAppApp.swift
+//  OnboardingiOSApp
+//
+//  Created by dedeepya reddy salla on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
